13a91c003c54468a314e677694b0ec884afa708a
aadarsh73 <agarwalaadarshx73@gmail.com>
2024-01-31 01:30:37 +0530

    lab2 done, calculator partially left

--- lab2/program3/app/src/main/java/com/example/program3/MainActivity2.kt
+++ lab2/program3/app/src/main/java/com/example/program3/MainActivity2.kt
@@ -4,15 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
-
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
 
-//        val intent = Intent(this, MainActivity2::class.java)
-//        startActivity(intent)
-
-
+        val intent = Intent()
     }
 }
