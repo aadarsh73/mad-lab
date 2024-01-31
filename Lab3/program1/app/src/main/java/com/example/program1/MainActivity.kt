dd4bb791d694fdab7d7560cb93da1ff3a7e73e21
aadarsh73 <agarwalaadarshx73@gmail.com>
2024-01-31 12:25:54 +0530

    lab3 rest

--- Lab3/program1/app/src/main/java/com/example/program1/MainActivity.kt
+++ Lab3/program1/app/src/main/java/com/example/program1/MainActivity.kt
@@ -11,28 +11,34 @@ import android.widget.TextView
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.recyclerview.widget.RecyclerView.LayoutManager
+import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val listView = findViewById<ListView>(R.id.listView)
-        val cities = arrayOf(
-            "New York", "Los Angeles", "Chicago", "Houston", "Philadelphia", "Phoenix", "San Antonio", "San Diego",
-            "Dallas", "San Jose", "Austin", "Jacksonville", "San Francisco", "Indianapolis", "Columbus", "Fort Worth",
-            "Charlotte", "Seattle", "Denver", "El Paso", "Detroit", "Washington D.C.", "Boston", "Memphis", "Nashville",
-            "Portland", "Oklahoma City", "Las Vegas", "Baltimore", "Louisville", "Milwaukee", "Albuquerque", "Tucson",
-            "Fresno", "Sacramento", "Kansas City", "Long Beach", "Mesa", "Atlanta", "Colorado Springs", "Virginia Beach",
-            "Raleigh", "Omaha", "Miami", "Oakland", "Minneapolis", "Tulsa", "Bakersfield", "Arlington", "Wichita"
-        )
-        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, cities)
-        listView.adapter = adapter
+//        val listView = findViewById<ListView>(R.id.listView)
+//        val cities = arrayOf(
+//            "New York", "Los Angeles", "Chicago", "Houston", "Philadelphia", "Phoenix", "San Antonio", "San Diego",
+//            "Dallas", "San Jose", "Austin", "Jacksonville", "San Francisco", "Indianapolis", "Columbus", "Fort Worth",
+//            "Charlotte", "Seattle", "Denver", "El Paso", "Detroit", "Washington D.C.", "Boston", "Memphis", "Nashville",
+//            "Portland", "Oklahoma City", "Las Vegas", "Baltimore", "Louisville", "Milwaukee", "Albuquerque", "Tucson",
+//            "Fresno", "Sacramento", "Kansas City", "Long Beach", "Mesa", "Atlanta", "Colorado Springs", "Virginia Beach",
+//            "Raleigh", "Omaha", "Miami", "Oakland", "Minneapolis", "Tulsa", "Bakersfield", "Arlington", "Wichita"
+//        )
+//        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, cities)
+//        listView.adapter = adapter
 
         val recyclerView = findViewById<RecyclerView>(R.id.recycler_view)
-// Use a GridLayoutManager with 2 columns
         recyclerView.layoutManager = GridLayoutManager(this, 2)
         recyclerView.adapter = CustomAdapter(generateItems())
+
+
+        val tabLayout = findViewById<TabLayout>(R.id.tab_layout)
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 1"))
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 2"))
+        tabLayout.addTab(tabLayout.newTab().setText("Tab 3"))
 
     }
 }
